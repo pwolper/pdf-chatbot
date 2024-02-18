627795c03b477fb1d98ab0889adfca76de57bbc1
pwolper <philip.wolper@gmail.com>
2024-02-18 23:09:03 +0100

    conversational function implemented

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 import os, sys, time, re
+import base64
 import streamlit as st
 
 from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.schema import Document
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_experimental.text_splitter import SemanticChunker
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain, create_history_aware_retriever
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+#from langchain_experimental.text_splitter import SemanticChunker
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain_community.vectorstores import faiss
 
@@ -29,12 +36,10 @@ def get_api_key():
             st.session_state.openai_api_key=os.getenv("OPENAI_API_KEY")
             return
 
-
-
 def process_pdfs():
     loader = PyPDFDirectoryLoader("data/")
     docs = loader.load_and_split()
-    st.write(str(str(len(docs))+ " documents extracted"))
+    #st.write(str(str(len(docs))+ " documents extracted"))
     return(docs)
 
 def chunk_text(docs):
@@ -42,7 +47,7 @@ def chunk_text(docs):
     for p in range(0, len(docs)):
         text = docs[p].page_content
         single_sentence_list = re.split(r"(?<=[.?!])\s+", text)
-        print(f"{len(single_sentence_list)} sentences were found in {docs[p].metadata}")
+        #print(f"{len(single_sentence_list)} sentences were found in {docs[p].metadata['source']} on page {docs[p].metadata['page']}")
         for i in range(0, len(single_sentence_list)-3, 3):
             chunk = single_sentence_list[i:i+4]
             chunk = " ".join(chunk)
@@ -55,16 +60,60 @@ def chunk_text(docs):
 def create_vectorstore(chunks):
         embeddings = OpenAIEmbeddings()
         vectorstore = faiss.FAISS.from_documents(chunks, embeddings)
+        vectorstore.save_local("vector_db", index_name="pdf")
         return(vectorstore)
 
-#@st.cache_data(show_spinner="Summarizing the document...")
-def summary_chain(chunks, openai_api_key):
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo-1106", temperature=0.5, openai_api_key=openai_api_key)
-    chain = load_summarize_chain(llm=llm, chain_type="stuff")
-    summarization = chain.invoke(chunks)
-    return(summarization)
+def get_context_retriever_chain(vectorstore):
+    llm = ChatOpenAI()
+    retriever = vectorstore.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return(retriever_chain)
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI()
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's question in a brief manner based on the context below:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user","{input}")
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return(create_retrieval_chain(retriever_chain, stuff_documents_chain))
+
+def get_response(user_query):
+    retriever_chain = get_context_retriever_chain(vector_db)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+    response = conversation_rag_chain.invoke({
+        "chat_history": st.session_state.chat_history,
+        "input": user_query
+    })
+
+    return(response['answer'])
 
 
+def displayPDF(file):
+    # Opening file from file path
+    with open(file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+
+    # Embedding PDF in HTML
+    pdf_display =  f"""<embed
+    class="pdfobject"
+    type="application/pdf"
+    title="Embedded PDF"
+    src="data:application/pdf;base64,{base64_pdf}"
+    style="overflow: auto; width: 100%; height: 100%;">"""
+
+    # Displaying File
+    st.markdown(pdf_display, unsafe_allow_html=True)
 
 
 ### Streamlit page starts here ###
@@ -74,13 +123,40 @@ st.header("PDF Chatbot")
 
 get_api_key()
 
-for paper in os.listdir("data/"):
-    st.write(paper)
+# for paper in os.listdir("data/"):
+#     st.write(paper)
 
+## Processing pdfs
 docs = process_pdfs()
 chunks = chunk_text(docs)
+#st.write(f"Number of resulting text chunks: {len(chunks)}")
 
-    # chunks.append
+## Creating vector store
+#create_vectorstore(chunks)
+vector_db = faiss.FAISS.load_local("vector_db", embeddings = OpenAIEmbeddings(), index_name="pdf")
+
+displayPDF("data/johri-2022-recom-improv.pdf")
 
 
-# chunks = semantic_chunking(docs)
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a helpful AI expert. How can I help you?"),]
+
+
+user_query = st.chat_input("Type your message here...")
+if user_query is not None and user_query != "":
+    response = get_response(user_query)
+    st.session_state.chat_history.append(HumanMessage(content=user_query))
+    st.session_state.chat_history.append(AIMessage(content=response))
+
+for message in st.session_state.chat_history:
+    if isinstance(message, AIMessage):
+        with st.chat_message("AI"):
+            st.write(message.content)
+    if isinstance(message, HumanMessage):
+        with st.chat_message("Human"):
+            st.write(message.content)
+
+
+with st.sidebar:
+    st.write(st.session_state.chat_history)
