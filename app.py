c535a9276041523d079384849c905a172063efa1
pwolper <philip.wolper@gmail.com>
2024-05-17 16:13:27 +0200

    added streamling, formatting and code refactoring

--- app.py
+++ app.py
@@ -3,28 +3,19 @@
 import os, sys, time, re
 import base64
 import streamlit as st
+import streamlit.components.v1 as components
+import streamlit as st
 from streamlit_pdf_viewer import pdf_viewer
 from streamlit_float import *
-import bibtexparser
 
-# Langchain imports
-from langchain_community.chat_models import ChatOpenAI
 from langchain_core.messages import AIMessage, HumanMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain.schema import Document
-from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import ConversationalRetrievalChain, create_history_aware_retriever
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
-#from langchain_experimental.text_splitter import SemanticChunker
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain_community.vectorstores import faiss
+from functions import parse_bibtex
+from functions import process_dir, process_file, chunk_text
+from functions import create_vectorstore, load_vectorstore
+from functions import get_context_retriever_chain, get_conversational_rag_chain
+from functions import get_response, llm_network_call, json_parsing, pyvis_graph
 
 
-
-### Functions ###
 def get_api_key():
     if "openai_api_key" not in st.session_state:
         if not os.getenv("OPENAI_API_KEY"):
@@ -41,109 +32,9 @@ def get_api_key():
             st.session_state.openai_api_key=os.getenv("OPENAI_API_KEY")
             return
 
-def process_dir():
-    loader = PyPDFDirectoryLoader("data/")
-    docs = loader.load_and_split()
-    #st.write(str(str(len(docs))+ " documents extracted"))
-    return(docs)
-
-def process_file(file):
-    loader = PyPDFLoader(file)
-    docs = loader.load_and_split()
-    #st.write(str(str(len(docs))+ " documents extracted"))
-    print('file processed...')
-    return(docs)
-
-def chunk_text(docs):
-    documents = []
-    for p in range(0, len(docs)):
-        text = docs[p].page_content
-        single_sentence_list = re.split(r"(?<=[.?!])\s+", text)
-        #print(f"{len(single_sentence_list)} sentences were found in {docs[p].metadata['source']} on page {docs[p].metadata['page']}")
-        for i in range(0, len(single_sentence_list)-3, 3):
-            chunk = single_sentence_list[i:i+4]
-            chunk = " ".join(chunk)
-            doc = Document(page_content=chunk, metadata=docs[p].metadata)
-            documents.append(doc)
-    return(documents)
-
-#@st.cache_data()
-def create_vectorstore(chunks, file):
-        embeddings = OpenAIEmbeddings()
-        vectorstore = faiss.FAISS.from_documents(chunks, embeddings)
-        vectorstore.save_local("vector_db", index_name=pdf)
-        return(vectorstore)
-
-def get_context_retriever_chain(vectorstore):
-    llm = ChatOpenAI()
-    retriever = vectorstore.as_retriever()
-    prompt = ChatPromptTemplate.from_messages([
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user", "{input}"),
-        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
-    ])
-
-    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
-
-    return(retriever_chain)
-
-def get_conversational_rag_chain(retriever_chain):
-    llm = ChatOpenAI(model='gpt-4')
-    prompt = ChatPromptTemplate.from_messages([
-        ("system", "Answer the user's question in a brief manner based on the context below:\n\n{context}"),
-        MessagesPlaceholder(variable_name="chat_history"),
-        ("user","{input}")
-    ])
-    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
-
-    return(create_retrieval_chain(retriever_chain, stuff_documents_chain))
-
-def get_response(user_query):
-    retriever_chain = get_context_retriever_chain(vector_db)
-    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
-
-    response = conversation_rag_chain.invoke({
-        "chat_history": st.session_state.chat_history,
-        "input": user_query
-    })
-
-    return(response['answer'])
-
-
-def displayPDF(file):
-    pdf_viewer(file, width=800)
-    # # Opening file from file path
-    # with open(file, "rb") as f:
-    #     base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-
-    # # Embedding PDF in HTML
-    # pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-
-    # # Displaying File
-    # st.markdown(pdf_display, unsafe_allow_html=True)
-    return
-
-def parse_bibtex(file="articles.bib"):
-    with open(file, 'r') as bib:
-        library = bibtexparser.load(bib)
-
-        articles = []
-        for entry in library.entries:
-            info = {}
-            info['title'] = entry['title']
-            info['authors'] = entry['author']
-            info['pdf'] = str(entry['ID'] + ".pdf")
-            info['year'] = entry['year']
-            info['url'] = entry['url']
-            articles.append(info)
-    return(articles)
-
-
-
 ### Streamlit page starts here ###
 
-st.set_page_config(page_title="PDF Chatbot", page_icon=":books:", initial_sidebar_state="collapsed", layout="wide")
-st.markdown("**pdf-chatbot: Question AI models about papers, while reading...**")
+st.set_page_config(page_title="PDF Chatbot", page_icon=":books:", initial_sidebar_state="expanded", layout="wide")
 
 float_init()
 
@@ -151,56 +42,71 @@ get_api_key()
 
 articles = parse_bibtex()
 
-a, b, c = st.columns([2,0.1,0.5])
-with a:
+
+with st.sidebar:
+    st.sidebar.title("PDF chatbot")
+    st.markdown("**Question AI models about articles and generate knowledge graphs to enhance text understanding. Powered by OpenAI API and LangChain.**")
+
+
+# a, b, c = st.columns([2,0.1,0.5])
+# with a:
     article = st.selectbox("Choose an article",
                            [a['title'] for a in articles],
                            index = None,
                            label_visibility="collapsed")
 
-    if article is not None:
-        dir = "data/"
-        pdf = str([a['pdf'] for a in articles if a['title'] == article][0])
-        file = dir + pdf
+    st.markdown("**About**")
+    st.markdown("Created by Philip Wolper ([phi.wolper@gmail.com](philip.wolper@gmail.com)). Code is available at [https://github.com/pwolper/scimapai.git](https://github.com/pwolper/pdf-chatbot) here. Feeback is very welcome.")
 
-with b:
-    #st.button("Display PDF", type="primary")
-    st.write('or')
+if article is not None:
+    dir = "data/"
+    pdf = str([a['pdf'] for a in articles if a['title'] == article][0])
+    file = dir + pdf
 
-with c:
-    uploader = st.button("Upload a file")
-    if uploader:
-        upload = st.file_uploader(label="Upload your pdf",
-                                label_visibility="collapsed")
-        if upload is not None:
-            # To read file as bytes:
-            bytes = upload.getvalue()
-            new_file = open('data/tmp/new.pdf', 'wb')
-            new_file.write(bytes)
-            article = 'User uploaded file'
-            file = 'data/tmp/new.pdf'
-            pdf = 'new.pdf'
-            st.write(article)
+######## with b:
+########     #st.button("Display PDF", type="primary")
+########     st.write('or')
+
+######## with c:
+########     uploader = st.button("Upload a file")
+########     if uploader:
+########         upload = st.file_uploader(label="Upload your pdf",
+########                                 label_visibility="collapsed")
+########         if upload is not None:
+########             # To read file as bytes:
+########             bytes = upload.getvalue()
+########             new_file = open('data/tmp/new.pdf', 'wb')
+########             new_file.write(bytes)
+########             article = 'User uploaded file'
+########             file = 'data/tmp/new.pdf'
+########             pdf = 'new.pdf'
+########             st.write(article)
 
 
 if article is None:
+    st.session_state.chat_history = [
+        AIMessage(content="Hello, I am a helpful AI expert. How can I help you?"),]
+    source_code = None
     st.stop()
 
 
 ## Processing pdfs
-docs = process_file(file)
-chunks = chunk_text(docs)
+# docs = process_file(file)
+# chunks = chunk_text(docs)
 
-st.write(f"Number of resulting text chunks: {len(chunks)}")
+#st.write(f"Number of resulting text chunks: {len(chunks)}")
 
 
 ## Creating vector store
 if str(pdf + ".faiss") not in os.listdir("vector_db"):
     st.toast(":page_facing_up: Generating embeddings...")
+    docs = process_file(file)
+    chunks = chunk_text(docs)
     vector_db = create_vectorstore(chunks,file)
+
 else:
-    # st.toast(f"Embeddings found for the file {pdf} :smile:")
-    vector_db = faiss.FAISS.load_local("vector_db", embeddings = OpenAIEmbeddings(), index_name=pdf)
+    #st.toast(f"Embeddings found for the file {pdf} :smile:")
+    vector_db = load_vectorstore(pdf)
 
 
 if "chat_history" not in st.session_state:
@@ -210,26 +116,20 @@ if "chat_history" not in st.session_state:
 if "retrieved" not in st.session_state:
     st.session_state.retrieved = None
 
-col1, col2 = st.columns([1.4,1], gap="small")
-
-with col1:
-    displayPDF(file)
+col1, col2 = st.columns([1.3,1], gap="small")
 
 with col2:
-    container = st.container()
-    with container:
-        st.write("")
-        #st.write("")
-        #header = str([a['title'] + ' (' + a['authors'] + ', ' + a['year'] + ')' for a in articles if a['title'] == article][0])
-        header = article
-        st.markdown(str('**' + header + '**'))
-        chat = st.container(height=350)
-        user_query = st.chat_input("Type your message here...")
-
-        if user_query is not None and user_query != "":
-            response = get_response(user_query)
-            st.session_state.chat_history.append(HumanMessage(content=user_query))
-            st.session_state.chat_history.append(AIMessage(content=response))
+    tab1, tab2 = st.tabs(["GPT-4", "Knowledge Graph"])
+    with tab1:
+        container = st.container()
+        with container:
+            st.write("")
+            #st.write("")
+            #header = str([a['title'] + ' (' + a['authors'] + ', ' + a['year'] + ')' for a in articles if a['title'] == article][0])
+            header = article
+            st.markdown(str('**' + header + '**'))
+            chat = st.container(height=400)
+            user_query = st.chat_input("Type your message here...")
 
         # Print chat_history to chat
         for message in st.session_state.chat_history:
@@ -239,12 +139,40 @@ with col2:
             if isinstance(message, HumanMessage):
                 with chat.chat_message("Human"):
                     st.write(message.content)
-    container.float()
-    # container.button("Start", type="primary")
 
+        if user_query is not None and user_query != "":
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
 
-with st.sidebar:
-    with st.expander("chat history"):
-        st.write(st.session_state.chat_history)
-    with st.expander("Retrieved chunks for last prompt"):
-        st.write(st.session_state.retrieved)
+            with chat.chat_message("Human"):
+                st.markdown(user_query)
+
+            with chat.chat_message("AI"):
+                response = get_response(user_query, st.session_state.chat_history, vector_db)
+                streamed_response = st.write_stream(response)
+
+            st.session_state.chat_history.append(AIMessage(content=streamed_response))
+        container.float()
+
+    with tab2:
+        if st.session_state.chat_history != [AIMessage(content="Hello, I am a helpful AI expert. How can I help you?"),]:
+            mapping_output = llm_network_call(st.session_state.chat_history)
+            nodes, edges = json_parsing(mapping_output)
+            source_code=pyvis_graph(nodes, edges)
+            st.markdown("**Knowledge Graph:**")
+            components.html(source_code, height=500,width=600)
+            download=st.download_button("Download HTML", data=source_code, file_name="knowledge_graph.html")
+
+            with st.sidebar.expander("Debug"):
+                st.write(mapping_output)
+                st.write(nodes)
+                st.write(edges)
+   # container.button("Start", type="primary")
+
+with col1:
+    display_container = st.container()
+    with display_container:
+        # displayPDF(file)
+        pdf_viewer(file, width=700, height=1000)
+
+    display_container.float()
+
---
+++ functions.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+
+import re, time
+from typing import List
+import json
+from pyvis.network import Network
+from pyvis import network as net
+import bibtexparser
+# Langchain imports
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema import Document
+from langchain_community.document_loaders import PyPDFDirectoryLoader, PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain, create_history_aware_retriever
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+#from langchain_experimental.text_splitter import SemanticChunker
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_community.vectorstores import faiss
+
+def process_dir():
+    loader = PyPDFDirectoryLoader("data/")
+    docs = loader.load_and_split()
+    #st.write(str(str(len(docs))+ " documents extracted"))
+    return(docs)
+
+def process_file(file):
+    loader = PyPDFLoader(file)
+    docs = loader.load_and_split()
+    #st.write(str(str(len(docs))+ " documents extracted"))
+    #print('file processed...')
+    return(docs)
+
+def chunk_text(docs):
+    documents = []
+    for p in range(0, len(docs)):
+        text = docs[p].page_content
+        single_sentence_list = re.split(r"(?<=[.?!])\s+", text)
+        #print(f"{len(single_sentence_list)} sentences were found in {docs[p].metadata['source']} on page {docs[p].metadata['page']}")
+        for i in range(0, len(single_sentence_list)-3, 3):
+            chunk = single_sentence_list[i:i+4]
+            chunk = " ".join(chunk)
+            doc = Document(page_content=chunk, metadata=docs[p].metadata)
+            documents.append(doc)
+    return(documents)
+
+#@st.cache_data()
+def create_vectorstore(chunks, file):
+        embeddings = OpenAIEmbeddings()
+        vectorstore = faiss.FAISS.from_documents(chunks, embeddings)
+        vectorstore.save_local("vector_db", index_name=pdf)
+        return(vectorstore)
+
+def load_vectorstore(index_name):
+    vectorstore = faiss.FAISS.load_local("vector_db",
+                                         embeddings = OpenAIEmbeddings(),
+                                         index_name = index_name)
+    return(vectorstore)
+
+def get_context_retriever_chain(vectorstore):
+    llm = ChatOpenAI()
+    retriever = vectorstore.as_retriever()
+    prompt = ChatPromptTemplate.from_messages([
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user", "{input}"),
+        ("user", "Given the above conversation, generate a search query to look up in order to get information relevant to the conversation")
+    ])
+
+    retriever_chain = create_history_aware_retriever(llm, retriever, prompt)
+
+    return(retriever_chain)
+
+def get_conversational_rag_chain(retriever_chain):
+    llm = ChatOpenAI(model='gpt-4')
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "Answer the user's question in a brief manner based on the context below:\n\n{context}"),
+        MessagesPlaceholder(variable_name="chat_history"),
+        ("user","{input}")
+    ])
+    stuff_documents_chain = create_stuff_documents_chain(llm, prompt)
+
+    return(create_retrieval_chain(retriever_chain, stuff_documents_chain))
+
+def get_response(user_query, chat_history, vector_db):
+    retriever_chain = get_context_retriever_chain(vector_db)
+    conversation_rag_chain = get_conversational_rag_chain(retriever_chain)
+
+
+    response = conversation_rag_chain.stream({"chat_history": chat_history,
+                                              "input": user_query})
+    for chunk in response:
+        content = chunk.get("answer", "")
+        yield content
+
+def llm_network_call(chat_history):
+    llm = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0)
+
+    # mapping_template = """
+    # You are an scientific assistant, tasked with extracting the main concepts and key words from articles and abstracts. I want to create a concept map to enhance understanding of the text.
+    # Breaking down the text hierarchically and semantically, I want you to choose high-level concepts as nodes, avoiding overlap between concepts. Keep the number of nodes to a minimum.
+    # Concepts should be as atomistic as possible, but chosen so that there is high connectivity in the graph.
+    # Return nodes and edges for the concept map and come up with a sentence explaining each edge as well.
+    # Text: {text}
+
+    # Strictly return a list of json objects, with the following fields:
+    # "node_1", "node_2" and "edge". Where "node_1" and "node_2", represent two nodes and "edge" is a string containing a sentence describing the relationship between the nodes.
+    # Do not wrap the output in ```json ```.
+    # """
+
+    mapping_template = """
+    You are an assistant, tasked with extracting the main concepts and topics from conversation. I want to create a concept map to enhance understanding of the complex topics.
+    Breaking down the conversation hierarchically and semantically, I want you to choose high-level concepts as nodes, avoiding overlap between concepts. Keep the number of nodes to a minimum.
+
+    Concepts should be as atomistic as possible, but chosen so that there is high connectivity in the graph.
+    Return nodes and edges for the concept map and come up with a short sentence explaining each edge as well.
+    Text: {text}
+
+    Strictly return a list of json objects, with the following fields:
+    "node_1", "node_2" and "edge". Where "node_1" and "node_2", represent two nodes and "edge" is a string containing a sentence describing the relationship between the nodes.
+    Do not wrap the output in ```json ```.
+    """
+    mapping_prompt = ChatPromptTemplate.from_messages(
+        [("system", mapping_template),
+         ("human", "{text}"),
+         ])
+
+    messages = mapping_prompt.format_messages(text=chat_history)
+    answer = llm(messages)
+    output = answer.content
+    return(output)
+
+def json_parsing(mapping_output):
+    output_dict = json.loads(mapping_output)
+
+    nodes = []
+    edges = []
+
+    for dict in output_dict:
+        node_1 = dict['node_1']
+        node_2 = dict['node_2']
+        edge = dict['edge']
+
+        nodes.append(node_1)
+        nodes.append(node_2)
+
+        edges.append((node_1, node_2, edge))
+    return(nodes, edges)
+
+def pyvis_graph(nodes, edges):
+    nt = Network(directed=False,
+                 notebook=True,height="499px",width="599px",
+                #height="480px",
+                #width="620px",
+                #width="940px",
+                heading='')
+
+    for n in nodes:
+        nt.add_node(n,
+                    title=n,
+                    size=15)
+
+    for source, target, label in edges:
+        nt.add_edge(source,
+                    target,
+                    title=label)
+    # nt.barnes_hut()
+    nt.show('pyvis_knowledge_graph.html')
+    html_file = open('./pyvis_knowledge_graph.html', 'r', encoding='utf-8')
+    source_code = html_file.read()
+    return(source_code)
+
+def parse_bibtex(file="articles.bib"):
+    with open(file, 'r') as bib:
+        library = bibtexparser.load(bib)
+
+        articles = []
+        for entry in library.entries:
+            info = {}
+            info['title'] = entry['title']
+            info['authors'] = entry['author']
+            info['pdf'] = str(entry['ID'] + ".pdf")
+            info['year'] = entry['year']
+            info['url'] = entry['url']
+            articles.append(info)
+    return(articles)
